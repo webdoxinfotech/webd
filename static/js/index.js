e9a39ae0b99d71157df12107ed1679b710b3fa02
webshivdev <webdevshivam@gmail.com>
2024-01-03 17:55:35 +0530

    updated css

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+import Typed from "./typed.js";
+
+const auto = new Typed('.auto-text', {
+  strings: ['Concept Based Practical Training', '5+ Years of Experience', 'Concept Based Practical Training', 'Learning By Doing'],
+  loop: true,
+  IPColor: 'white'
+});
+
+// Initialize Typing Effect
+auto.start();
